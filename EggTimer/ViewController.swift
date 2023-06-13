21d3efaa095dcc33d2fccb531d5d843da117ea53
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-13 20:46:10 +0100

    Added timer function for when one of the timer buttons is clicked - counts back from seconds given by dictionary entry

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -11,13 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     let eggTimes = [
-        "Soft": 5,
-        "Medium": 7,
-        "Hard": 12
+        "Soft": 300,
+        "Medium": 420,
+        "Hard": 720
     ]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
-        print(eggTimes[hardness]!)
+        guard let hardness = sender.currentTitle else
+        {
+            return
+        }
+        
+        guard let timer = eggTimes[hardness] else { return }
+        
+        runTimer(seconds: timer)
+    }
+    
+    func runTimer(seconds: Int) {
+        var secondsRemaining = seconds
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            secondsRemaining -= 1
+            print(secondsRemaining)
+            
+            if secondsRemaining == 0 {
+                timer.invalidate()
+            }
+        }
     }
 }
